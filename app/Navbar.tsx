db2ebce17922a2c1ff3f86346d0198f6f689d578
Zain Ali <zainali187483@gmail.com>
2025-04-28 17:20:00 +0500

    ✨  Style the active link in the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,19 +1,32 @@
+'use client';
 import Link from 'next/link';
 import React from 'react';
-import { FaBug } from "react-icons/fa";
-
-
+import { FaBug } from 'react-icons/fa';
+import { usePathname } from 'next/navigation';
+import classnames from 'classnames';
 const Navbar = () => {
   const navLinks = [
     { label: 'Dashboard', href: '/' },
     { label: 'Issues', href: '/issues' },
   ];
+  const currentPath = usePathname();
   return (
-    <nav className='flex items-center space-x-6 h-14 px-5 border-b mb-5 '>
-      <Link href="/"><FaBug/></Link>
-      <ul className='flex space-x-6'>
-        {navLinks.map((link) => (
-          <li key={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors'>
+    <nav className="flex items-center space-x-6 h-14 px-5 border-b mb-5 ">
+      <Link href="/">
+        <FaBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {/* classnames is a library that allows you to conditionally add classes in more flexible/cleaner way */}
+        {navLinks.map(link => (
+          <li
+            key={link.href}
+            className={classnames({
+            'text-zinc-900' : link.href===currentPath,
+            'text-zinc-500': link.href!==currentPath,
+            'hover:text-zinc-800 transition-colors' : true
+            })
+            }
+          >
             <Link href={link.href}>{link.label}</Link>
           </li>
         ))}
