cde17e39d8d378fd2c0b1376a099e39c46bc93d0
Zain Ali <zainali187483@gmail.com>
2025-05-04 15:53:20 +0500

    💄  Change the layout of the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { FaBug } from 'react-icons/fa';
 import { usePathname } from 'next/navigation';
 import classnames from 'classnames';
-import { Box } from '@radix-ui/themes';
+import { Box, Container, Flex } from '@radix-ui/themes';
 import { useSession } from 'next-auth/react';
 import { stat } from 'fs';
 const Navbar = () => {
@@ -15,33 +15,39 @@ const Navbar = () => {
   const currentPath = usePathname();
   const { status, data: session } = useSession();
   return (
-    <nav className="flex items-center space-x-6 h-14 px-5 border-b border-gray-300 mb-5 ">
-      <Link href="/">
-        <FaBug />
-      </Link>
-      <ul className="flex space-x-6">
-        {/* classnames is a library that allows you to conditionally add classes in more flexible/cleaner way */}
-        {navLinks.map(link => (
-          <li
-            key={link.href}
-            className={classnames({
-              'text-zinc-900': link.href === currentPath,
-              'text-zinc-500': link.href !== currentPath,
-              'hover:text-zinc-800 transition-colors': true,
-            })}
-          >
-            <Link href={link.href}>{link.label}</Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status==='authenticated' && (
-          <Link href='/api/auth/signout'>Log out</Link>
-        )}
-        {status==='unauthenticated' && (
-          <Link href='/api/auth/signin'>Log in</Link>
-        )}
-      </Box>
+    <nav className="py-3 px-5 border-b border-gray-300 mb-5 ">
+      <Container>
+        <Flex justify={'between'}>
+          <Flex align="center" gap={'3'}>
+            <Link href="/">
+              <FaBug />
+            </Link>
+            <ul className="flex space-x-6">
+              {/* classnames is a library that allows you to conditionally add classes in more flexible/cleaner way */}
+              {navLinks.map(link => (
+                <li
+                  key={link.href}
+                  className={classnames({
+                    'text-zinc-900': link.href === currentPath,
+                    'text-zinc-500': link.href !== currentPath,
+                    'hover:text-zinc-800 transition-colors': true,
+                  })}
+                >
+                  <Link href={link.href}>{link.label}</Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === 'authenticated' && (
+              <Link href="/api/auth/signout">Log out</Link>
+            )}
+            {status === 'unauthenticated' && (
+              <Link href="/api/auth/signin">Log in</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
