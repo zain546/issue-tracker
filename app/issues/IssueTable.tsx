0b9a360d83727811526ac391d27001543d7a476c
Zain Ali <zainali187483@gmail.com>
2025-05-07 18:18:56 +0500

    🔨  Refactor: Extract IssueTable

---
+++ app/issues/IssueTable.tsx
@@ -0,0 +1,71 @@
+import { Issue, Status } from '@prisma/client';
+import { ArrowUpIcon } from '@radix-ui/react-icons';
+import { Table } from '@radix-ui/themes';
+import { default as Link, default as NextLink } from 'next/link';
+import { IssueStatusBadge } from '../components';
+
+export interface IssueQuery{
+    status: Status;
+    orderBy: keyof Issue;
+    page: string;
+    
+}
+interface Props {
+  searchParams: Promise<IssueQuery>;
+  issues: Issue[];
+}
+const IssueTable = async({ searchParams, issues}: Props) => {
+    const params = await searchParams;
+  return (
+    <Table.Root variant="surface">
+      <Table.Header>
+        <Table.Row>
+          {columns.map(column => (
+            <Table.ColumnHeaderCell
+              key={column.value}
+              className={column.classname}
+            >
+              <NextLink
+                href={{
+                  query: { ...params, orderBy: column.value },
+                }}
+              >
+                {column.label}
+              </NextLink>
+              {column.value === params.orderBy && (
+                <ArrowUpIcon className="inline" />
+              )}
+            </Table.ColumnHeaderCell>
+          ))}
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {issues.map(issue => (
+          <Table.Row key={issue.id}>
+            <Table.Cell>
+              <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+              <div className="block md:hidden text-xs">
+                <IssueStatusBadge status={issue.status} />
+              </div>
+            </Table.Cell>
+            <Table.Cell className="hidden md:table-cell">
+              <IssueStatusBadge status={issue.status} />
+            </Table.Cell>
+            <Table.Cell className="hidden md:table-cell">
+              {issue.createdAt.toDateString()}
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+  );
+};
+
+export default IssueTable;
+
+const columns: { label: string; value: keyof Issue; classname?: string }[] = [
+    { label: 'Issue', value: 'title' },
+    { label: 'Status', value: 'status', classname: 'hidden md:table-cell' },
+    { label: 'Created', value: 'createdAt', classname: 'hidden md:table-cell' },
+  ];
+  export const columnValues = columns.map(column => column.value);
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,36 +1,25 @@
 import { prisma } from '@/prisma/client';
-import { Table } from '@radix-ui/themes';
-import { Link, IssueStatusBadge } from '@/app/components';
-import IssuesActions from './IssuesActions';
-import { Issue, Status } from '@prisma/client';
-import NextLink from 'next/link';
-import { ArrowUpIcon } from '@radix-ui/react-icons';
+import { Status } from '@prisma/client';
 import Pagination from '../components/Pagination';
+import IssuesActions from './IssuesActions';
+import IssueTable, { columnValues, IssueQuery } from './IssueTable';
+import { Flex } from '@radix-ui/themes';
 interface Props {
-  searchParams: Promise<{
-    status: Status;
-    orderBy: keyof Issue;
-    page: string;
-  }>;
+  searchParams: Promise<IssueQuery>;
 }
+
 export default async function IssuePage({ searchParams }: Props) {
-  const columns: { label: string; value: keyof Issue; classname?: string }[] = [
-    { label: 'Issue', value: 'title' },
-    { label: 'Status', value: 'status', classname: 'hidden md:table-cell' },
-    { label: 'Created', value: 'createdAt', classname: 'hidden md:table-cell' },
-  ];
   const params = await searchParams;
   const statuses = Object.values(Status);
   const validateStatus = statuses.includes(params.status)
     ? params.status
     : undefined;
-  const columnValues = columns.map(column => column.value);
   const validateorderBy = columnValues.includes(params.orderBy)
     ? { [params.orderBy]: 'asc' }
     : undefined;
-    const page = Number(params.page) || 1;
-    const pageSize = 10;
-    const where = {status: validateStatus};
+  const page = Number(params.page) || 1;
+  const pageSize = 10;
+  const where = { status: validateStatus };
   const issues = await prisma.issue.findMany({
     where,
     orderBy: validateorderBy,
@@ -39,55 +28,15 @@ export default async function IssuePage({ searchParams }: Props) {
   });
   const issueCount = await prisma.issue.count({ where });
   return (
-    <div>
+    <Flex direction="column" gap="3">
       <IssuesActions />
-      <Table.Root variant="surface">
-        <Table.Header>
-          <Table.Row>
-            {columns.map(column => (
-              <Table.ColumnHeaderCell
-                key={column.value}
-                className={column.classname}
-              >
-                <NextLink
-                  href={{
-                    query: { ...params, orderBy: column.value },
-                  }}
-                >
-                  {column.label}
-                </NextLink>
-                {column.value === params.orderBy && (
-                  <ArrowUpIcon className="inline" />
-                )}
-              </Table.ColumnHeaderCell>
-            ))}
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {issues.map(issue => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                <div className="block md:hidden text-xs">
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+      <IssueTable searchParams={searchParams} issues={issues} />
       <Pagination
         itemCount={issueCount}
         pageSize={pageSize}
         currentPage={page}
       />
-    </div>
+    </Flex>
   );
 }
 // export const revalidate = 0; means no cache, it will revalidate every time the page is visited, it's purpose is to make sure the data is up to date when the page is visited
