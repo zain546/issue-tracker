8fb60a0c1b2576a131058a9746c3d4521b18ef69
Zain Ali <zainali187483@gmail.com>
2025-05-08 11:09:48 +0500

    ✨  Build the IssueSummary component

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,51 @@
+import { Status } from '@prisma/client';
+import { Badge, Card, Flex, Text } from '@radix-ui/themes';
+import Link from 'next/link';
+import React from 'react';
+
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+
+const IssueSummary = ({ open, inProgress, closed }: Props) => {
+  const issues: {
+    label: string;
+    value: number;
+    status: Status;
+  }[] = [
+    { label: 'Open Issues', value: open, status: 'OPEN' },
+    { label: 'In_Progress Issues', value: inProgress, status: 'IN_PROGRESS' },
+    { label: 'Closed Issues', value: closed, status: 'CLOSED' },
+  ];
+  return (
+    <Flex gap="4">
+      {issues.map(issue => (
+        <Card key={issue.label}>
+          <Flex direction="column" align="start" gap="2">
+            <Link
+              className="text-sm font-semibold"
+              href={`/issues?status=${issue.status}`}
+            >
+              {issue.label}
+            </Link>
+            <Badge
+              color={
+                issue.status === 'OPEN'
+                  ? 'red'
+                  : issue.status === 'IN_PROGRESS'
+                  ? 'plum'
+                  : 'green'
+              }
+            >
+              {issue.value}
+            </Badge>
+          </Flex>
+        </Card>
+      ))}
+    </Flex>
+  );
+};
+
+export default IssueSummary;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import LatestIssues from './LatestIssues';
+import { prisma } from '@/prisma/client';
+import IssueSummary from './IssueSummary';
 
 export default async function Home() {
-  return <LatestIssues />;
+  const open = await prisma.issue.count({ where: { status: 'OPEN' } });
+  const inProgress = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' },
+  });
+  const closed = await prisma.issue.count({ where: { status: 'CLOSED' } });
+  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
 }
