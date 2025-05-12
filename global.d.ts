47cc12f5a714e6bb43db3fc3c0e90b64d6ccfdf9
Zain Ali <zainali187483@gmail.com>
2025-05-12 22:45:33 +0500

    🐛  Fix the JSX type error in production

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,9 @@
-/// <reference types="react" />
+// global.d.ts
+import type { JSX as ReactJSX } from 'react';
+
+declare global {
+  namespace JSX {
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+    interface IntrinsicElements extends ReactJSX.IntrinsicElements {}
+  }
+}
